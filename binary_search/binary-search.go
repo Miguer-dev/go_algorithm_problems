f9f9fab686344f2d8ce6dcc76880f78487b9db94
Miguer-dev <miguerdev@gmail.com>
2024-08-27 11:31:26 -0400

    add binary-search

---
+++ binary_search/binary-search.go
@@ -0,0 +1,15 @@
+package binary_search
+
+func BinarySearch(nums []int, target int, passlen int) int {
+	if len(nums) == 0 {
+		return -1
+	} else if nums[len(nums)/2] == target {
+		return len(nums)/2 + passlen
+	} else if nums[len(nums)/2] > target {
+		return BinarySearch(nums[:len(nums)/2], target, passlen)
+	} else if nums[len(nums)/2] < target {
+		return BinarySearch(nums[len(nums)/2+1:], target, (len(nums)/2+1)+passlen)
+	}
+
+	return -1
+}
